5d2edcb2e13b5e0fa5e9fbe29be7cb51a6889cc0
Tanat0s <berserk.bellato@yandex.ru>
2021-12-06 23:37:56 +0300

    pinball
    Add table and a few  obstecles.

---
+++ Assets/scripts/SpringScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var direction = transform.position - other.transform.position;
+
+        other.attachedRigidbody.AddForce(new Vector3(0,0, 2), ForceMode.Impulse);
+    }
+}
---
+++ Assets/scripts/WallObstecleScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallObstecleScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var direction = transform.position - other.transform.position;
+
+        other.attachedRigidbody.AddForce(direction*-5, ForceMode.Impulse);
+    }
+}
